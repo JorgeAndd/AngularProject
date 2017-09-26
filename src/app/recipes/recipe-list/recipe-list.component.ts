b7901ddba997fdfe8274307f737622fbbb339bd2
JorgeAndd <jorgeluizandrade@outlook.com>
2017-09-26 10:14:49 -0300

    Displaying recipe list using ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Rice', 'Just rice', 'https://upload.wikimedia.org/wikipedia/commons/d/d6/A_bowl_of_rice.jpg')
+  ];
 
   constructor() { }
 
